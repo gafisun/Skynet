db525422ee472ec355abfe342e56ee9244f25018
baget <77748891+gafisun@users.noreply.github.com>
2022-01-30 13:44:00 +0300

    RFID-модуль
    - Подключил RFID-модуль
    - Разграничил разделы кода

--- skynet.ino
+++ skynet.ino
@@ -1,9 +1,10 @@
-
 #include <GyverOLED.h>
-#include <Adafruit_MLX90614.h>#include <Adafruit_MLX90614.h>
+#include <Adafruit_MLX90614.h>
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heartRate.h"
+#include <MFRC522.h> // Подключение для библиотеки под  RFID модуль
+#include <SPI.h>
 MAX30105 particleSensor;
 
 
@@ -16,13 +17,26 @@ MAX30105 particleSensor;
 //int beatAvg;
 
 
-GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
+GyverOLED<SSD1306_128x64, OLED_BUFFER> oled; // Настройка OLED экрана
 //Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
+constexpr uint8_t RST_PIN = 0;          // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 2;         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600); // Старт монитора
+
+ // ====================== RFID-модуль ==================
+    SPI.begin();      // Init SPI bus
+    mfrc522.PCD_Init();    // Init MFRC522
+    mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+    Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+ // ====================== RFID-модуль ================== 
+ 
+  //  mlx.begin();
 //
 //    if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
 //    {
@@ -36,20 +50,21 @@ void setup() {
 //    particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
 //    particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
-  oled.init();
-  mlx.begin();
+// ======================= ДИСПЛЕЙ ===================================
+  oled.init(); // Инициализация олед дисплея
 
-  oled.clear();
-  oled.update();
+
+  oled.clear(); // Очистка OLED-дисплея
+  oled.update(); // Отправка сигнала на oled дисплей
 
   oled.home();
 
   oled.setScale(2);
   oled.autoPrintln(false);
-  oled.print("Temperatur");
-  oled.setScale(2);
-  oled.print("e");
+  oled.print("Temperat");
   oled.update();
+// ======================= ДИСПЛЕЙ ===================================
+
 }
 
 void loop() {
@@ -78,6 +93,18 @@ void loop() {
 //                beatAvg += rates[x];
 //            beatAvg /= RATE_SIZE;
 //        }
-    }
 
+// ====================== RFID-модуль ==================  
+// Look for new cards Поиск новых карт
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  
+// Select one of the cards Выбор карты
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+    // Dump debug info about the card; PICC_HaltA() is automatically called 
+   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+ // ====================== RFID-модуль ==================
 }
