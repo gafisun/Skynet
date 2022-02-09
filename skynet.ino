11d461f58985cb68ea0d7f78de90c1c90d8e9327
”Baget” <gafisun01@gmail.com>
2022-02-09 21:50:31 +0300

    Первые шаги по созданию режимов пользования

--- skynet.ino
+++ skynet.ino
@@ -1,4 +1,4 @@
- // =================== ДРУГОЕ =================
+// =================== ДРУГОЕ =================
 #include "heartRate.h"
 
 
@@ -34,31 +34,31 @@ byte masterCard[4];   // Stores master card's ID read from EEPROM
 
 ///////////////////////////////////////// Setup ///////////////////////////////////
 void setup() {
-// ============== Protocol Configuration =============
-    Serial.begin(9600); // Старт монитора
-if (!ml.begin()) {
+  // ============== Protocol Configuration =============
+  Serial.begin(9600); // Старт монитора
+  if (!ml.begin()) {
     Serial.println("Error connecting to MLX sensor. Check wiring.");
     while (1);
   };
 
-// ====================== RFID-модуль ==================
-    SPI.begin();      // Init SPI bus. MFRC522 Hardware uses SPI protocol
-    mfrc522.PCD_Init();    // Initialize MFRC522 Hardware 
-    mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-    Serial.println("Scan PICC to see UID, SAK, type, and data blocks..."); // Печать в мониторе строки готовности RFID модуля к считыванию данных
-
- 
-// ================= СИСТЕМА ИНДЕНТИФИКАЦИИ =============
-Serial.println("Access Control Example v0.1");   // For debugging purposes
-//   ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
+  // ====================== RFID-модуль ==================
+  SPI.begin();      // Init SPI bus. MFRC522 Hardware uses SPI protocol
+  mfrc522.PCD_Init();    // Initialize MFRC522 Hardware
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println("Scan PICC to see UID, SAK, type, and data blocks..."); // Печать в мониторе строки готовности RFID модуля к считыванию данных
 
 
-// ======================= ДИСПЛЕЙ ===================================
+  // ================= СИСТЕМА ИНДЕНТИФИКАЦИИ =============
+  Serial.println("Access Control Example v0.1");   // For debugging purposes
+  //   ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
+
+
+  // ======================= ДИСПЛЕЙ ===================================
   oled.init(); // Старт OLED дисплея
 
   oled.clear(); // Очистка OLED дисплея от прошлых команд
   oled.update(); // Обновление OLED дисплея
-  
+
   oled.home(); // Возвращение дисплея в точку (0, 0)
 
   oled.setScale(2); // Размер Шрифта равен 2 единицам
@@ -67,16 +67,16 @@ Serial.println("Access Control Example v0.1");   // For debugging purposes
   oled.update(); // Обновление OLED дисплеяы
 
 
-// ============================================ САТУРАЦИЯ ============================================
-//Setup to sense up to 18 inches, max LED brightness
+  // ============================================ САТУРАЦИЯ ============================================
+  //Setup to sense up to 18 inches, max LED brightness
   byte ledBrightness = 0xFF; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
   int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411; //Options: 69, 118, 215, 411
   int adcRange = 2048; //Options: 2048, 4096, 8192, 16384
-  
-if (particleSensor.begin() == false)
+
+  if (particleSensor.begin() == false)
   {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
@@ -85,47 +85,65 @@ if (particleSensor.begin() == false)
 
 
 
-// ============================================ КНОПКА ============================================
-pinMode( D0, INPUT);
-pinMode( LED_BUILTIN, OUTPUT);
+  // ============================================ КНОПКА ============================================
+  pinMode( D0, INPUT);
+  pinMode( LED_BUILTIN, OUTPUT);
 
-
+  // ============================================ РЕЖИМЫ РАБОТЫ ============================================
 }
 
 ///////////////////////////////////////// Main Loop ///////////////////////////////////
+byte mode_check = 0;
 void loop() {
-  
-// =============== ВЫВОД ТЕМПЕРАТУРЫ =========================
-  oled.setCursor(0, 4);
-  oled.setScale(3);
-oled.print(String((int)ml.readObjectTempC()));
-  oled.update();
 
-// ====================== RFID-модуль ==================  
-// Look for new cards Поиск новых карт
+  if (mode_check == 0){
+    
+      // =============== ВЫВОД ТЕМПЕРАТУРЫ ========================= 
+      oled.setCursor(0, 4);
+      oled.setScale(3);
+      oled.print(String((int)ml.readObjectTempC()));
+      oled.update();
+      
+    } 
+    if (mode_check == 1){
+
+      oled.clear(); // Очистка OLED дисплея от прошлых команд
+      oled.update(); // Обновление OLED дисплея
+
+      oled.home(); // Возвращение дисплея в точку (0, 0)
+
+      oled.setScale(2); // Размер Шрифта равен 2 единицам
+      oled.print("Сатурация"); // Печать пробного текста
+      oled.update(); // Обновление OLED дисплеяы
+
+       // ============================================ САТУРАЦИЯ ============================================
+       particleSensor.check();
+       particleSensor.nextSample();
+    }
+  
+  // ====================== RFID-модуль ==================
+  // Look for new cards Поиск новых карт
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-  
-// Select one of the cards Выбор карты
+
+  // Select one of the cards Выбор карты
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-    // Dump debug info about the card; PICC_HaltA() is automatically called 
-   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
-// ============================================ САТУРАЦИЯ ============================================
-particleSensor.check();
-particleSensor.nextSample();
-
-
-// ============================================ КНОПКА ============================================
-byte val =  digitalRead(D1);
-
-if (val == HIGH){
-  digitalWrite(LED_BUILTIN, LOW);
-} else {
-  digitalWrite(LED_BUILTIN, HIGH);
-}
  
+
+
+  // ============================================ КНОПКА ============================================
+  byte val =  digitalRead(D1);
+  
+  if (val == HIGH) {
+    mode_check = 0;
+  } else {
+    mode_check = 1;
+  }
+
 }
