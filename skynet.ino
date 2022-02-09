1aec7757945d400c860d66fbf615337db4a48cec
”Baget” <gafisun01@gmail.com>
2022-02-09 21:18:16 +0300

    Добавил кнопку, которая включает встроенный светоидиод.

--- skynet.ino
+++ skynet.ino
@@ -1,22 +1,21 @@
  // =================== ДРУГОЕ =================
 #include "heartRate.h"
-//  =============== ДРУГОЕ ==================
+
 
 // ============================================ САТУРАЦИЯ ============================================
 #include <Wire.h> // Подключение библиотеки проводов
 #include "MAX30105.h" // Подключение библиотеки для датчика сатурации
 MAX30105 particleSensor; // Создания объекта "датчик температуры"
-// ============================================ САТУРАЦИЯ ============================================
 
 // ============== ТЕМПЕРАТУРА =================
 #include <Adafruit_MLX90614.h> // Подключение библиотеки для датчика температуры
 Adafruit_MLX90614 ml = Adafruit_MLX90614();
-// ============== ТЕМПЕРАТУРА =================
+
 
 // ============ OLED =============
 #include <GyverOLED.h> // Подключение Библиотеки для OLED дисплея
 GyverOLED<SSD1306_128x64, OLED_BUFFER> oled; // Создания объекта дисплея с нужынми характеристиками
-// ============ OLED =============
+
 
 // ================== RFID ================
 #include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
@@ -31,13 +30,12 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 byte storedCard[4];   // Stores an ID read from EEPROM
 byte readCard[4];   // Stores scanned ID read from RFID Module
 byte masterCard[4];   // Stores master card's ID read from EEPROM
-// ============ RFID =============
+
 
 ///////////////////////////////////////// Setup ///////////////////////////////////
 void setup() {
 // ============== Protocol Configuration =============
     Serial.begin(9600); // Старт монитора
-// ============== Protocol Configuration =============
 if (!ml.begin()) {
     Serial.println("Error connecting to MLX sensor. Check wiring.");
     while (1);
@@ -48,12 +46,12 @@ if (!ml.begin()) {
     mfrc522.PCD_Init();    // Initialize MFRC522 Hardware 
     mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
     Serial.println("Scan PICC to see UID, SAK, type, and data blocks..."); // Печать в мониторе строки готовности RFID модуля к считыванию данных
-// ====================== RFID-модуль ================== 
+
  
 // ================= СИСТЕМА ИНДЕНТИФИКАЦИИ =============
 Serial.println("Access Control Example v0.1");   // For debugging purposes
 //   ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
-// ================= СИСТЕМА ИНДЕНТИФИКАЦИИ ==============
+
 
 // ======================= ДИСПЛЕЙ ===================================
   oled.init(); // Старт OLED дисплея
@@ -67,7 +65,7 @@ Serial.println("Access Control Example v0.1");   // For debugging purposes
   oled.autoPrintln(false); // Отключение автоматического переноса строки
   oled.print("Прив"); // Печать пробного текста
   oled.update(); // Обновление OLED дисплеяы
-// ======================= ДИСПЛЕЙ ===================================
+
 
 // ============================================ САТУРАЦИЯ ============================================
 //Setup to sense up to 18 inches, max LED brightness
@@ -84,7 +82,14 @@ if (particleSensor.begin() == false)
     while (1);
   }
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-// ============================================ САТУРАЦИЯ ============================================
+
+
+
+// ============================================ КНОПКА ============================================
+pinMode( D0, INPUT);
+pinMode( LED_BUILTIN, OUTPUT);
+
+
 }
 
 ///////////////////////////////////////// Main Loop ///////////////////////////////////
@@ -95,7 +100,6 @@ void loop() {
   oled.setScale(3);
 oled.print(String((int)ml.readObjectTempC()));
   oled.update();
-// =============== ВЫВОД ТЕМПЕРАТУРЫ =========================
 
 // ====================== RFID-модуль ==================  
 // Look for new cards Поиск новых карт
@@ -109,11 +113,19 @@ oled.print(String((int)ml.readObjectTempC()));
   }
     // Dump debug info about the card; PICC_HaltA() is automatically called 
    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
- // ====================== RFID-модуль ==================
 
 // ============================================ САТУРАЦИЯ ============================================
 particleSensor.check();
 particleSensor.nextSample();
-// ============================================ САТУРАЦИЯ ============================================
+
+
+// ============================================ КНОПКА ============================================
+byte val =  digitalRead(D1);
+
+if (val == HIGH){
+  digitalWrite(LED_BUILTIN, LOW);
+} else {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
  
 }
